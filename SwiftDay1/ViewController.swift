2d93044cf2ce3bfca49945f8861c6fc6fa3fe3d4
xsoullament <xiesiqiang@xiesiqiangdemacbook-pro.local>
2017-12-08 17:42:05 +0800

    dya1

--- SwiftDay1/ViewController.swift
+++ SwiftDay1/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var number: UILabel!
+    
+    @IBOutlet var palyButton: UIButton!
+    
+    @IBOutlet var pauseButton: UIButton!
+    
+    var timer: Timer?
+    var conut = 0.0
+    
+    var isPlay = false{
+        didSet{
+            palyButton.isEnabled = isPlay
+            pauseButton.isEnabled = !isPlay
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        isPlay = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startAction(_ sender: Any) {
+        isPlay = false
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateNum), userInfo: nil, repeats: true)
     }
+    
+    @IBAction func pauseAction(_ sender: UIButton) {
+        isPlay = true
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @IBAction func resetAcrion(_ sender: Any) {
+        isPlay = true
+        timer?.invalidate()
+        timer = nil
+        conut = 0
+        number.text = "\(conut)"
+    }
+    
+    @objc func updateNum(){
+        conut += 0.1
+        number.text = String(format: "%.1f", conut)
 
-
+    }
+    
 }
 
